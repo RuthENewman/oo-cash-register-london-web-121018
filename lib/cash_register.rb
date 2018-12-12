553c7920e3371799fb874a5021f1cd3676c9c6e3
ruthnewman <ruthnewman@gmail.com>
2018-12-12 17:30:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
-
+class CashRegister
+  
+  attr_accessor :total
+  
+  def initialize
+    @total = 0
+  end 
+  
+  def add_item(title, price, )
+    @total += 1
+  end 
+  
+  def apply_discount
+    
+  end 
+  
+  def 
+  
+end
